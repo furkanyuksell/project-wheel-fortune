bfe613e60d0505fb260cb1afc8c7945a81b92700
Furkan YÜKSEL <furkan.yuksel3@std.yildiz.edu.tr>
2025-08-23 14:25:26 +0300

    Added : content to panels

--- Assets/Scripts/Gameplay/PanelSystem/Base/BasePanel.cs
+++ Assets/Scripts/Gameplay/PanelSystem/Base/BasePanel.cs
@@ -6,6 +6,6 @@ namespace Gameplay.PanelSystem.Base
     public abstract class BasePanel : BaseBehaviour
     {
         [Header("References")]
-        [SerializeField] private Transform _container;
+        [SerializeField] private Transform _content;
     }
 }
